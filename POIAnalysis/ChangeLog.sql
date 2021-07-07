dd5cf98d0ed97fd6c59788555359d4d9e41457fc
Regan Sarwas <regan_sarwas@nps.gov>
2021-07-06 19:32:09 -0800

    Make Changes to POI and facilities for reconciliation

---
+++ POIAnalysis/ChangeLog.sql
@@ -0,0 +1,153 @@
+-- AKR_SOCIO database cleanup
+
+-- Change Log
+-- 2021-07-06
+
+-- Update the source DB values for buiidings (2512 fixes)
+update akr_socio.gis.POI_PT
+set SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT', SRCDBIDFLD = 'FEATUREID', srcdbnmfld = 'BLDGNAME'
+where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building'
+
+-- Update the source DB values for trail feature points (271 fixes)
+update akr_socio.gis.POI_PT
+set SRCDBNAME = 'akr_facility2.GIS.TRAILS_FEATURE_PT', SRCDBIDFLD = 'GEOMETRYID', srcdbnmfld = 'TRLFEATNAME'
+where SRCDBNAME = 'akr_facility.GIS.TRAILS_Feature_pt'
+
+-- Normalize the building and trail GUIDs (42 fixes)
+update akr_socio.GIS.POI_PT SET SRCDBIDVAL = '{' + upper(SRCDBIDVAL) + '}' 
+where SRCDBNAME like 'akr%'
+and len(SRCDBIDVAL) = 36
+
+-- Normalize the FEATUREID GUIDs (42 fixes)
+update akr_socio.GIS.POI_PT SET FEATUREID = '{' + upper(FEATUREID) + '}' 
+where len(FEATUREID) = 36
+
+-- Normalize the GEOMETRYID GUIDs (2 fixes)
+update akr_socio.GIS.POI_PT SET GEOMETRYID = '{' + upper(GEOMETRYID) + '}' 
+where len(GEOMETRYID) = 36
+
+-- Replace empty strings with nulls
+select count(*) from akr_socio.gis.POI_PT where SRCDBNMVAL = ''
+Update akr_socio.gis.POI_PT set SRCDBNAME = null where SRCDBNAME = '' -- 11 fixes
+Update akr_socio.gis.POI_PT set SRCDBIDFLD = null where SRCDBIDFLD = '' -- 1 fix
+Update akr_socio.gis.POI_PT set SRCDBNMFLD = null where SRCDBNMFLD = '' -- 1 fix
+Update akr_socio.gis.POI_PT set SRCDBIDVAL = null where SRCDBIDVAL = '' -- 1 fix
+Update akr_socio.gis.POI_PT set SRCDBNMVAL = null where SRCDBNMVAL = '' -- 2 fix
+
+-- Repeat the previous commands on the history table to keep it is sync (it is only updated when posintg a version to default)
+-- At this point the history table is "identical" to the base table, so this is safe
+-- In the future all changes hsould be made to a version so the changes are captured in the history
+
+-- Update the source DB values for buiidings
+update akr_socio.gis.POI_PT_H
+set SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT', SRCDBIDFLD = 'FEATUREID', srcdbnmfld = 'BLDGNAME'
+where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building'
+
+-- Update the source DB values for trail feature points
+update akr_socio.gis.POI_PT_H
+set SRCDBNAME = 'akr_facility2.GIS.TRAILS_FEATURE_PT', SRCDBIDFLD = 'GEOMETRYID', srcdbnmfld = 'TRLFEATNAME'
+where SRCDBNAME = 'akr_facility.GIS.TRAILS_Feature_pt'
+
+-- Normalize the building and trail GUIDs (42 fixes)
+update akr_socio.GIS.POI_PT_H SET SRCDBIDVAL = '{' + upper(SRCDBIDVAL) + '}' 
+where SRCDBNAME like 'akr%'
+and len(SRCDBIDVAL) = 36
+
+-- Normalize the FEATUREID GUIDs (42 fixes)
+update akr_socio.GIS.POI_PT_H SET FEATUREID = '{' + upper(FEATUREID) + '}' 
+where len(FEATUREID) = 36
+
+-- Normalize the GEOMETRYID GUIDs (2 fixes)
+update akr_socio.GIS.POI_PT_H SET GEOMETRYID = '{' + upper(GEOMETRYID) + '}' 
+where len(GEOMETRYID) = 36
+
+-- Replace empty strings with nulls
+--select count(*) from akr_socio.gis.POI_PT where SRCDBNMVAL = ''
+Update akr_socio.gis.POI_PT_H set SRCDBNAME = null where SRCDBNAME = '' -- 11 fixes
+Update akr_socio.gis.POI_PT_H set SRCDBIDFLD = null where SRCDBIDFLD = '' -- 1 fix
+Update akr_socio.gis.POI_PT_H set SRCDBNMFLD = null where SRCDBNMFLD = '' -- 1 fix
+Update akr_socio.gis.POI_PT_H set SRCDBIDVAL = null where SRCDBIDVAL = '' -- 1 fix
+Update akr_socio.gis.POI_PT_H set SRCDBNMVAL = null where SRCDBNMVAL = '' -- 2 fix
+
+
+-- Create archive of POITYPES
+-- The POI points for buildings that are no public map display will be deleted
+-- and the POITYPE value will not be transfered to the BLDG table (BLDGS with a non-null POITYPE should be added to POI points)
+select SRCDBNAME, SRCDBIDVAL, POITYPE into akr_socio.DBO.POITYPES_OF_FACILITY_ITEMS_REMOVED_FROM_POI_PT_20210706 from akr_socio.GIS.POI_PT  where SRCDBNAME like 'akr%' and PUBLICDISPLAY = 'No Public Map Display' 
+
+
+-- Buildings
+
+-- Create a list of map label updates that can be edited before being applied
+select p.Publicdisplay, b.FEATUREID, p.MAPLABEL as POI_MAPLABEL, '' as unused_POI_map_label, b.MAPLABEL AS BLDG_MAPLABEL, 'Y' as USE_POI, b.BLDGSTATUS 
+into akr_socio.dbo.bldg_map_label_updates
+from akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b
+left join akr_socio.gis.POI_PT as p
+-- POI_PT has duplicats that differ on ISExtant and/or iscurrentgeo
+on p.SRCDBIDVAL = b.FEATUREID and p.ISEXTANT = b.ISEXTANT
+where  SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
+-- only pick up the changes yo a non-null label value
+and p.MAPLABEL is not null and p.mapLABEL <> b.maplabel
+order by p.Publicdisplay, p.MAPLABEL
+
+-- Manually review and edit the use_POI column (Y/N), and update the POI_MAPLABEL (saving the original value in unused_POI_map_label)
+
+-- Update facilities version
+-- Create a version in akr_facilitiy2
+
+-- with select poi_maplabel from akr_socio.dbo.bldg_map_label_updates where USE_POI = 'Y'
+USE akr_facility2;
+
+DECLARE @version nvarchar(255) = 'dbo.res_poi_labels'
+exec sde.set_current_version @version
+exec sde.edit_version @version, 1 -- 1 to start edits
+
+-- can't update the view with joined data except via the merge command
+merge into gis.AKR_BLDG_CENTER_PT_evw as t1 using 
+    akr_socio.dbo.bldg_map_label_updates as t2
+    on t1.FEATUREID = t2.FEATUREID and t2.USE_POI = 'Y'
+    when matched then update set t1.MAPLABEL = t2.POI_MAPLABEL;
+
+exec sde.edit_version @version, 2; -- 2 to stop edits
+
+-- Trail Features
+exec sde.set_default
+-- Create a list of map label updates that can be edited before being applied
+select p.Publicdisplay, b.FEATUREID, p.MAPLABEL as POI_MAPLABEL, '' as unused_POI_map_label, b.MAPLABEL AS TRL_MAPLABEL, 'Y' as USE_POI 
+--into akr_socio.dbo.trail_map_label_updates
+from akr_facility2.gis.TRAILS_FEATURE_PT_evw as b
+left join akr_socio.gis.POI_PT as p
+-- POI_PT has duplicats that differ on ISExtant and/or iscurrentgeo
+on p.SRCDBIDVAL = b.GEOMETRYID and p.ISEXTANT = b.ISEXTANT
+where  SRCDBNAME = 'akr_facility2.GIS.TRAILS_FEATURE_PT'
+-- only pick up the changes yo a non-null label value
+and p.MAPLABEL is not null and p.mapLABEL <> b.maplabel
+order by p.Publicdisplay, p.MAPLABEL
+
+-- There is only one Trail point map label difference, so we will do it without an intermediate table
+select MAPLABEL from gis.TRAILS_FEATURE_PT_evw  where FeatureID = '{0C3502C1-C5F4-4E19-90F4-C448CA10DFD7}'
+
+DECLARE @version2 nvarchar(255) = 'dbo.res'
+exec sde.set_current_version @version2
+exec sde.edit_version @version2, 1 -- 1 to start edits
+update gis.TRAILS_FEATURE_PT_evw set MAPLABEL = 'Morino Trailhead' where FeatureID = '{0C3502C1-C5F4-4E19-90F4-C448CA10DFD7}'
+exec sde.edit_version @version2, 2; -- 2 to stop edits
+
+-- Do QC checks and post to default
+
+
+-- Create a version in Catalog for akr_socio
+-- exec sde.set_default
+-- exec sde.set_current_version 'dbo.res'
+-- 401 items from facilities have Public Map Display, and 2383 have No Public Map Display
+-- select count(*) from akr_socio.gis.akr_poi_pt_evw where SRCDBNAME like 'akr%' and PUBLICDISPLAY = 'No Public Map Display' 
+
+DECLARE @version3 nvarchar(255) = 'dbo.res'
+exec sde.set_current_version @version3
+exec sde.edit_version @version3, 1 -- 1 to start edits
+delete akr_socio.gis.akr_poi_pt_evw where SRCDBNAME like 'akr%' and PUBLICDISPLAY = 'No Public Map Display'
+exec sde.edit_version @version3, 2; -- 2 to stop edits
+
+
+
+
--- POIAnalysis/Compare_POI_Facilities.sql
+++ POIAnalysis/Compare_POI_Facilities.sql
@@ -22,16 +22,19 @@ where srcdbname like 'akr_facility%' group by srcdbname, POITYPE order by count(
 -- Normalize the building GUIDs
 select FEATUREID, '{' + upper(FEATUREID) + '}' from akr_socio.GIS.POI_PT
 -- update akr_socio.GIS.POI_PT SET FEATUREID = '{' + upper(FEATUREID) + '}' 
-where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building' and srcDBIDfld = 'BUILDING_ID'
+where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT' and srcDBIDfld = 'FEATUREID'
 and len(FEATUREID) = 36
 
 
--- POIs with broken links to buildings in akr_facilities (199 of 2512, 42 of which may be due to denormalized GUID)
+-- POIs with broken links to buildings in akr_facilities (159 of 2512)
 select p.FEATUREID, p.MAPLABEL from akr_socio.gis.POI_PT as p
 left join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
 on p.SRCDBIDVAL = b.FEATUREID
 where b.FEATUREID is null
-and p.SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building'
+and p.SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
+-- many broken links are due to removing iscurrentgeo = 'No' and ISEXTANT = 'False' from akr_facility2
+-- these will be deleted from POI_PTs, as will those with PUBLICDISPLAY = 'No Public Map Display'
+-- and p.ISEXTANT = 'True' and (ISCURRENTGEO = 'Yes' or ISCURRENTGEO is null) and p.PUBLICDISPLAY = 'Public Map Display'
 order by p.FEATUREID
 
 
@@ -46,18 +49,12 @@ and b.BLDGSTATUS = 'Existing'
 and b.ISOUTPARK = 'No'
 
 
--- Compare POIs with buildings (old facility link) (2313)
+-- Compare POIs with buildings (2313)
 select p.MAPLABEL, b.MAPlabel, p.poiname, b.BLDGNAME, p.poialtname, b.BLDGALTNAME, p.POITYPE, b.BLDGTYPE from akr_socio.gis.POI_PT as p
-join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
+left join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
 on p.SRCDBIDVAL = b.FEATUREID
-where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building'
-
--- Compare POIs with buildings (new facility link) (1)
-select p.MAPLABEL, b.MAPlabel, p.poiname, b.BLDGNAME, p.poialtname, b.BLDGALTNAME, p.POITYPE, b.BLDGTYPE from akr_socio.gis.POI_PT as p
-join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
-on p.SRCDBIDVAL = b.FEATUREID
-where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
-
+where p.SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
+order by p.SRCDBIDVAL
 
 -- building map labels that do not match poi map label (312)
 -- Recommend updating buidings map label with the value in POI map label
@@ -65,7 +62,7 @@ where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
 select p.MAPLABEL as [POI Map Label], b.MAPlabel as [buidling map label]from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
 on p.SRCDBIDVAL = b.FEATUREID
-where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building'
+where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
 and ((p.MAPLABEL is null and b.MAPLABEL is not null) or p.MAPLABEL <> b.MAPLABEL) --  or (p.MAPLABEL is not null and b.MAPLABEL is null))
 
 
@@ -75,7 +72,7 @@ and ((p.MAPLABEL is null and b.MAPLABEL is not null) or p.MAPLABEL <> b.MAPLABEL
 select p.poiname, b.BLDGNAME from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
 on p.SRCDBIDVAL = b.FEATUREID
-where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building'
+where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
 and ((p.POINAME is not null and b.bldgname is null) or p.POINAME <> b.BLDGNAME) -- or (p.POINAME is null and b.bldgname is not null))
 
 
@@ -84,17 +81,17 @@ and ((p.POINAME is not null and b.bldgname is null) or p.POINAME <> b.BLDGNAME) 
 select p.POITYPE, b.BLDGTYPE, count(*) as Qty from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
 on p.SRCDBIDVAL = b.FEATUREID
-where SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building' OR SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
+where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
 group by p.POITYPE, b.BLDGTYPE
 
--- Compare Location (305 issues, 50 significant)
+-- Compare Location (317 issues, 54 significant)
 select p.featureid, p.poiname, p.poitype,
 round((p.Shape.STY - b.Shape.STY)*111319,1) as Lat_Diff_m,
 round((p.Shape.STX - b.Shape.STX)*111319 * cos(p.Shape.STY*3.1415926/180),1) as Long_diff_m
 from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.AKR_BLDG_CENTER_PT_evw as b 
 on p.SRCDBIDVAL = b.FEATUREID
-where (SRCDBNAME = 'akr_facility.GIS.Building_Point, akr_facility.GIS.Building' OR SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT')
+where SRCDBNAME = 'akr_facility2.GIS.AKR_BLDG_CENTER_PT'
  and (p.Shape.STX <> b.Shape.STX or p.Shape.STY <> b.Shape.STY)
 order by abs(p.Shape.STX - b.Shape.STX)/2 + abs(p.Shape.STY - b.Shape.STY) desc
 
@@ -103,18 +100,18 @@ order by abs(p.Shape.STX - b.Shape.STX)/2 + abs(p.Shape.STY - b.Shape.STY) desc
 
 -- TRAIL FEATURE POINTS
 
--- Update the source DB values for buiidings
+-- Update the source DB values for trail feature points
 --update akr_socio.gis.POI_PT
---set SRCDBNAME = 'akr_facility2.GIS.TRAILS_Feature_pt', SRCDBIDFLD = 'GEOMETRYID', srcdbnmfld = 'TRLFEATNAME'
+--set SRCDBNAME = 'akr_facility2.GIS.TRAILS_FEATURE_PT', SRCDBIDFLD = 'GEOMETRYID', srcdbnmfld = 'TRLFEATNAME'
 --where SRCDBNAME = 'akr_facility.GIS.TRAILS_Feature_pt'
 
 
--- POIs with broken links to trail feature points in akr_facilities (59 out of 270)
+-- POIs with broken links to trail feature points in akr_facilities (59 out of 271)
 select p.FEATUREID, p.MAPLABEL from akr_socio.gis.POI_PT as p
 left join akr_facility2.gis.TRAILS_FEATURE_PT_evw as t 
 on p.SRCDBIDVAL = t.GEOMETRYID
 where t.GEOMETRYID is null
-and p.SRCDBNAME = 'akr_facility.GIS.TRAILS_Feature_pt'
+and p.SRCDBNAME = 'akr_facility2.GIS.TRAILS_Feature_pt'
 order by p.FEATUREID
 
 -- sample trail feature points
@@ -124,26 +121,26 @@ order by p.FEATUREID
 select p.MAPLABEL, t.MAPlabel, p.poiname, t.TRLFEATNAME, p.poialtname, t.TRLFEATALTNAME from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.TRAILS_FEATURE_PT_evw  as t
 on p.SRCDBIDVAL = t.GEOMETRYID
-where SRCDBNAME =  'akr_facility.GIS.TRAILS_Feature_pt'
+where SRCDBNAME =  'akr_facility2.GIS.TRAILS_Feature_pt'
 
 --Suggest overwrite the maplabel and trlfeatname in trail_feature_pt with the POI values
 select p.MAPLABEL as [POI map label], t.MAPlabel as [trail map label] from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.TRAILS_FEATURE_PT_evw  as t
 on p.SRCDBIDVAL = t.GEOMETRYID
-where SRCDBNAME =  'akr_facility.GIS.TRAILS_Feature_pt'
+where SRCDBNAME =  'akr_facility2.GIS.TRAILS_Feature_pt'
 and t.MAPLABEL is not null and p.maplabel <> t.maplabel
 
 select p.POINAME, TRLFEATNAME from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.TRAILS_FEATURE_PT_evw  as t
 on p.SRCDBIDVAL = t.GEOMETRYID
-where SRCDBNAME =  'akr_facility.GIS.TRAILS_Feature_pt'
+where SRCDBNAME =  'akr_facility2.GIS.TRAILS_Feature_pt'
 and t.TRLFEATNAME is not null and p.POINAME <> t.TRLFEATNAME
 
 -- Compare type
 select p.POITYPE, TRLFEATTYPE, count(*) as Qty from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.TRAILS_FEATURE_PT_evw  as t
 on p.SRCDBIDVAL = t.GEOMETRYID
-where SRCDBNAME =  'akr_facility.GIS.TRAILS_Feature_pt'
+where SRCDBNAME =  'akr_facility2.GIS.TRAILS_Feature_pt'
 group by p.POITYPE, TRLFEATTYPE
 
 -- Compare Location (35 issues, 9 significant)
@@ -153,5 +150,5 @@ round((p.Shape.STX - t.Shape.STX)*111319 * cos(p.Shape.STY*3.1415926/180),1) as 
 from akr_socio.gis.POI_PT as p
 join akr_facility2.gis.TRAILS_FEATURE_PT_evw  as t
 on p.SRCDBIDVAL = t.GEOMETRYID
-where SRCDBNAME =  'akr_facility.GIS.TRAILS_Feature_pt' and (p.Shape.STX <> t.Shape.STX or p.Shape.STY <> t.Shape.STY)
+where SRCDBNAME =  'akr_facility2.GIS.TRAILS_Feature_pt' and (p.Shape.STX <> t.Shape.STX or p.Shape.STY <> t.Shape.STY)
 order by abs(p.Shape.STX - t.Shape.STX)/2 + abs(p.Shape.STY - t.Shape.STY) desc
